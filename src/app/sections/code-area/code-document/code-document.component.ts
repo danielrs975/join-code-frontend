d96abee2deca60e0f3ba2ccac38b7deb8502cf64
Daniel <danielrs9705@gmail.com>
2020-01-10 18:03:24 +0100

    Formating documents

--- src/app/sections/code-area/code-document/code-document.component.ts
+++ src/app/sections/code-area/code-document/code-document.component.ts
@@ -6,157 +6,155 @@ import { ActivatedRoute } from '@angular/router';
 import { debounceTime } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-code-document',
-  templateUrl: './code-document.component.html',
-  styleUrls: ['./code-document.component.scss']
+	selector: 'app-code-document',
+	templateUrl: './code-document.component.html',
+	styleUrls: [ './code-document.component.scss' ]
 })
 export class CodeDocumentComponent implements OnInit {
-  
-  // We see the text editor to get the position of the cursor
-  @ViewChild(CodemirrorComponent, {read: '', static: true}) codeEditor: CodemirrorComponent;
+	// We see the text editor to get the position of the cursor
+	@ViewChild(CodemirrorComponent, { read: '', static: true })
+	codeEditor: CodemirrorComponent;
 
-  // This is the form of the document
-  documentForm: FormGroup
-  cm: any; // This is the instance of the code editor
+	// This is the form of the document
+	documentForm: FormGroup;
+	cm: any; // This is the instance of the code editor
 
-  // Seeing how it works
-  markers: any = {};
-  colors: any = {};
+	// Seeing how it works
+	markers: any = {};
+	colors: any = {};
 
-  // The id of the document open
-  docId;
-  
-  constructor(
-    private fb: FormBuilder,
-    private _documentService: DocumentService,
-    private _activatedRoute: ActivatedRoute
-  ) { 
-    this.docId = this._activatedRoute.snapshot.params.id
-  }
+	// The id of the document open
+	docId;
 
-  ngOnInit() {
-    // First a join the document
-    this._documentService.join(this._activatedRoute.snapshot.params.id);
+	constructor(
+		private fb: FormBuilder,
+		private _documentService: DocumentService,
+		private _activatedRoute: ActivatedRoute
+	) {
+		this.docId = this._activatedRoute.snapshot.params.id;
+	}
 
-    // Initialization of the document
-    this.documentForm = this.fb.group({
-      _id: [this._activatedRoute.snapshot.params.id],
-      modifyAt: [],
-      content: ['']
-    })
+	ngOnInit() {
+		// First a join the document
+		this._documentService.join(this._activatedRoute.snapshot.params.id);
 
-    // In here we listen when a document changes
-    this._documentService.listenUpdates().pipe(
-      debounceTime(50)
-    ).subscribe((doc) => {
-      if (this.cm) {
-        const realCoords = this.cm.getCursor();
-        this.documentForm.patchValue(doc, { emitEvent: false });
-        this.cm.setCursor(realCoords);
-      }
-      this.documentForm.patchValue(doc, { emitEvent: false });
-      this._documentService.usersInDocument.forEach((user) => {
-        this.setUserCursor(user, user.coords);
-      })
-    });
+		// Initialization of the document
+		this.documentForm = this.fb.group({
+			_id: [ this._activatedRoute.snapshot.params.id ],
+			modifyAt: [],
+			content: [ '' ]
+		});
 
-    // This listener listen when a user join to the document or leave it
-    this._documentService.listenUserJoinIn().subscribe((user) => {
-      this.setUserCursor(user, this.cm.getCursor());
-    })
-    this._documentService.listenUserLeaves().subscribe((user) => {
-      this.removeUserCursor(user);
-    })
-    this._documentService.listenToUserPosition().subscribe((users:any) => {
-      users.forEach((user) => this.setUserCursor(user, user['coords']))
-    })
-    this._documentService.updateMyPos.subscribe((user) => {
-      if (this.cm) this.cm.setCursor(user['coords']);
-    })
+		// In here we listen when a document changes
+		this._documentService.listenUpdates().pipe(debounceTime(50)).subscribe((doc) => {
+			if (this.cm) {
+				const realCoords = this.cm.getCursor();
+				this.documentForm.patchValue(doc, { emitEvent: false });
+				this.cm.setCursor(realCoords);
+			}
+			this.documentForm.patchValue(doc, { emitEvent: false });
+			this._documentService.usersInDocument.forEach((user) => {
+				this.setUserCursor(user, user.coords);
+			});
+		});
 
-    this.documentForm.valueChanges.subscribe((value) => {
-      this.updateCursorPos();
-      this.saveDocument(value)
-    });
-  }
-  /**
+		// This listener listen when a user join to the document or leave it
+		this._documentService.listenUserJoinIn().subscribe((user) => {
+			this.setUserCursor(user, this.cm.getCursor());
+		});
+		this._documentService.listenUserLeaves().subscribe((user) => {
+			this.removeUserCursor(user);
+		});
+		this._documentService.listenToUserPosition().subscribe((users: any) => {
+			users.forEach((user) => this.setUserCursor(user, user['coords']));
+		});
+		this._documentService.updateMyPos.subscribe((user) => {
+			if (this.cm) this.cm.setCursor(user['coords']);
+		});
+
+		this.documentForm.valueChanges.subscribe((value) => {
+			this.updateCursorPos();
+			this.saveDocument(value);
+		});
+	}
+	/**
    * This function save the document in the database
    * @param value The new content of the document
    */
-  saveDocument(value) {
-    setTimeout(() => {
-      this._documentService.save(value);
-    }, 5000);
-  }
+	saveDocument(value) {
+		setTimeout(() => {
+			this._documentService.save(value);
+		}, 5000);
+	}
 
-  /**
+	/**
    * This method get the event when the user change
    * the position of its cursor
    */
-  updateCursorPos() {
-    // console.log(this.documentForm);
-    if (this.cm) this._documentService.updateCursorPos(this.docId ,this.cm.getCursor());
-  }
+	updateCursorPos() {
+		// console.log(this.documentForm);
+		if (this.cm) this._documentService.updateCursorPos(this.docId, this.cm.getCursor());
+	}
 
-  ngAfterViewInit(): void {
-    this.cm = this.codeEditor.codeMirror;
-    this.cm.on("keydown", (cm, event) => {
-      if (this.isMovementKey(event.which)) {
-        setTimeout(() => {
-          this.updateCursorPos();
-        }, 10)
-      }
-    })
-    this.cm.on("mousedown", (cm) => {
-      setTimeout(() => {
-        this.updateCursorPos();
-      }, 10);
-    })
-  }
+	ngAfterViewInit(): void {
+		this.cm = this.codeEditor.codeMirror;
+		this.cm.on('keydown', (cm, event) => {
+			if (this.isMovementKey(event.which)) {
+				setTimeout(() => {
+					this.updateCursorPos();
+				}, 10);
+			}
+		});
+		this.cm.on('mousedown', (cm) => {
+			setTimeout(() => {
+				this.updateCursorPos();
+			}, 10);
+		});
+	}
 
-  /**
+	/**
    * In here we update the position of the cursor of a user
    * with a given id
    * @param coords The new coords of a user
    * @param id The id of the user
    */
-  private setUserCursor(user, coords) {
-    if (this.markers[user.socket_id]) this.markers[user.socket_id].clear();
-    // cm: CodeMirror instance
-    // cursorPos: The position of the cursor sent from another client ({line, ch} about CodeMirror)
-    
-    // Generate DOM node (marker / design you want to display)
-    const cursorCoords = this.cm.cursorCoords(true, coords);
+	private setUserCursor(user, coords) {
+		if (this.markers[user.socket_id]) this.markers[user.socket_id].clear();
+		// cm: CodeMirror instance
+		// cursorPos: The position of the cursor sent from another client ({line, ch} about CodeMirror)
 
-    const cursorElement = document.createElement('span');
-    cursorElement.style.animation = '1s blink step-end infinite'
-    cursorElement.style.borderLeftStyle = 'solid';
-    cursorElement.style.borderLeftWidth = '1.2px';
+		// Generate DOM node (marker / design you want to display)
+		const cursorCoords = this.cm.cursorCoords(true, coords);
 
-    if (!this.colors[user.socket_id]) {
-      this.colors[user.socket_id] = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-    }
+		const cursorElement = document.createElement('span');
+		cursorElement.style.animation = '1s blink step-end infinite';
+		cursorElement.style.borderLeftStyle = 'solid';
+		cursorElement.style.borderLeftWidth = '1.2px';
 
-    cursorElement.style.borderLeftColor = this.colors[user.socket_id]; 
-    cursorElement.style.height = `${(cursorCoords.bottom - cursorCoords.top)}px`;
-    cursorElement.style.padding = '0';
-    cursorElement.style.zIndex = '0'; 
-    
-    // Set the generated DOM node at the position of the cursor sent from another client
-    // setBookmark first argument: The position of the cursor sent from another client
-    // Second argument widget: Generated DOM node
-    this.markers[user.socket_id] = this.cm.setBookmark(coords, { widget: cursorElement });
-  }
+		if (!this.colors[user.socket_id]) {
+			this.colors[user.socket_id] = '#' + ((Math.random() * 0xffffff) << 0).toString(16);
+		}
 
-  /**
+		cursorElement.style.borderLeftColor = this.colors[user.socket_id];
+		cursorElement.style.height = `${cursorCoords.bottom - cursorCoords.top}px`;
+		cursorElement.style.padding = '0';
+		cursorElement.style.zIndex = '0';
+
+		// Set the generated DOM node at the position of the cursor sent from another client
+		// setBookmark first argument: The position of the cursor sent from another client
+		// Second argument widget: Generated DOM node
+		this.markers[user.socket_id] = this.cm.setBookmark(coords, { widget: cursorElement });
+	}
+
+	/**
    * Remove the cursor from the code editor
    * @param user The user that just leave the document
    */
-  private removeUserCursor(user) {
-    if (this.markers[user.socket_id]) this.markers[user.socket_id].clear();
-  }
+	private removeUserCursor(user) {
+		if (this.markers[user.socket_id]) this.markers[user.socket_id].clear();
+	}
 
-  private isMovementKey(keyCode) {
-    return 33 <= keyCode && keyCode <= 40;
-  }
+	private isMovementKey(keyCode) {
+		return 33 <= keyCode && keyCode <= 40;
+	}
 }
--- src/app/services/document.service.ts
+++ src/app/services/document.service.ts
@@ -3,110 +3,107 @@ import { Socket } from 'ngx-socket-io';
 import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class DocumentService {
+	private updates = new Subject();
+	private userJoin = new Subject();
+	private userChangePos = new Subject();
+	private userLeave = new Subject();
 
-  private updates = new Subject();
-  private userJoin = new Subject();
-  private userChangePos = new Subject();
-  private userLeave = new Subject();
+	updateMyPos = new Subject();
 
-  updateMyPos = new Subject();
+	usersInDocument: any = [];
+	socketId;
+	constructor(private socket: Socket) {
+		this.update();
+		this.listenNotification();
+	}
 
-  usersInDocument: any = [];
-  socketId;
-  constructor(
-    private socket: Socket
-  ) {
-    this.update();
-    this.listenNotification();
-  }
-
-  /**
+	/**
    * This function sends to the server the operation that
    * the user did in the document
    * @param operation The operation that the user did
    */
-  save(operation) {
-    this.socket.emit('save', operation);
-  }
+	save(operation) {
+		this.socket.emit('save', operation);
+	}
 
-  /**
+	/**
    * This function emit a event when a user join a document
    * @param docId The id of the document we want to join
    */
-  join(docId) {
-    this.socket.emit('join', {docId, coords: {line: 0, ch: 0, sticky: null}}, (mySocketId) => {
-      this.socketId = mySocketId;
-    });
-  }
+	join(docId) {
+		this.socket.emit('join', { docId, coords: { line: 0, ch: 0, sticky: null } }, (mySocketId) => {
+			this.socketId = mySocketId;
+		});
+	}
 
-  updateCursorPos(docId, coords) {
-    this.socket.emit('update-cursor-position', {docId, coords});
-  }
+	updateCursorPos(docId, coords) {
+		this.socket.emit('update-cursor-position', { docId, coords });
+	}
 
-  /**
+	/**
    * Return an observable to listen the updates
    * in the document
    */
-  listenUpdates() {
-    return this.updates;
-  }
+	listenUpdates() {
+		return this.updates;
+	}
 
-  /**
+	/**
    * Return an observable that listen when
    * a user join in to the document
    */
-  listenUserJoinIn() {
-    return this.userJoin;
-  }
+	listenUserJoinIn() {
+		return this.userJoin;
+	}
 
-  /**
+	/**
    * Return an observable that listen when
    * a user change their position in the document
    */
-  listenToUserPosition() {
-    return this.userChangePos;
-  }
+	listenToUserPosition() {
+		return this.userChangePos;
+	}
 
-  /**
+	/**
    * Return an observable that listen when
    * a user leaves the document
    */
-  listenUserLeaves() {
-    return this.userLeave;
-  }
+	listenUserLeaves() {
+		return this.userLeave;
+	}
 
-  // Listeners
-  /**
+	// Listeners
+	/**
    * This function listen the server in case of
    * new changes
    */
-  private update() {
-    this.socket.on('change', (update) => {
-      this.updates.next(update);
-    })
-  }
+	private update() {
+		this.socket.on('change', (update) => {
+			this.updates.next(update);
+		});
+	}
 
-  /**
+	/**
    * This function listen to notifications in the document
    */
-  private listenNotification() {
-    this.socket.on('notification', (not) => {
-      console.log(not);
-    })
+	private listenNotification() {
+		this.socket.on('notification', (not) => {
+			console.log(not);
+		});
 
-    this.socket.on('user-new-position', (users) => {
-      this.usersInDocument = users.filter((user) => user.socket_id !== this.socketId);
-      this.userChangePos.next(this.usersInDocument);
-      const user = users.find((user) => user.socket_id === this.socketId);
-      if (user) this.updateMyPos.next(user);
-    });
+		this.socket.on('user-new-position', (users) => {
+			this.usersInDocument = users.filter((user) => user.socket_id !== this.socketId);
+			this.userChangePos.next(this.usersInDocument);
+			const user = users.find((user) => user.socket_id === this.socketId);
+			if (user) this.updateMyPos.next(user);
+		});
 
-    this.socket.on('user-leave', ({users, userRemoved}) => {
-      this.usersInDocument = users.filter((user) => user.socket_id !== this.socketId);
-      this.userLeave.next(userRemoved);
-    })
-  }
+		this.socket.on('user-leave', ({ users, userRemoved }) => {
+			this.usersInDocument = users.filter((user) => user.socket_id !== this.socketId);
+			this.userLeave.next(userRemoved);
+		});
+	}
 }
