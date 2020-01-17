40e0f6276dca9749dd33bbaa57145f5708d9b850
angelicacosta <14-10005@usb.ve>
2020-01-17 12:27:11 +0100

    Adding dashboard view

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { CodeDocumentComponent } from '../sections/code-area/code-document/code-document.component';
+import { DashboardComponent } from '../sections/dashboard/dashboard.component';
 
 const routes: Routes = [
-    { path: 'document/:id', component: CodeDocumentComponent }
+    { path: 'document/:id', component: CodeDocumentComponent },
+    { path: 'dashboard', component: DashboardComponent }
 ];
 
 @NgModule({
