55c729fabb5c47f5d56bc516d93102765d7bfcbb
Daniel <danielrs9705@gmail.com>
2020-01-22 15:39:59 +0100

    Partially done the view of the dashboard

--- src/app/sections/dashboard/dashboard.component.ts
+++ src/app/sections/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from 'src/app/services/users.service';
 import { forkJoin } from 'rxjs';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
 	selector: 'app-dashboard',
@@ -12,7 +14,7 @@ export class DashboardComponent implements OnInit {
 	userDocuments: any;
 	sharedDocuments: any;
 
-	constructor(private usersService: UsersService) {}
+	constructor(private usersService: UsersService, private router: Router, private auth: AuthService) {}
 
 	ngOnInit() {
 		// Getting profile info and documents info at the same time
@@ -21,5 +23,21 @@ export class DashboardComponent implements OnInit {
 			this.userDocuments = responses[1]['documents']; // User own documents
 			this.sharedDocuments = responses[1]['sharedDocuments']; // User shared documents
 		});
+	}
+
+	/**
+	 * This method open a document in the editor 
+	 * @param id The id of the document to open
+	 */
+	openDoc(id: string) {
+		this.router.navigate([ 'document', id ]);
+	}
+
+	/**
+	 * This method logout the user from the app
+	 */
+	logOut() {
+		this.auth.logout().subscribe((response) => console.log(response), (e) => console.log('Error has ocurred!', e));
+		this.router.navigate([ '/login' ]);
 	}
 }
