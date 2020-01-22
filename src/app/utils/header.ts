098a0c480dab6356f04f86f48f83ad7e8e2dd821
Daniel <danielrs9705@gmail.com>
2020-01-22 12:23:50 +0100

    Put some comments to the code

--- src/app/utils/header.ts
+++ src/app/utils/header.ts
@@ -1,5 +1,9 @@
 import { HttpHeaders } from '@angular/common/http';
 
+/**
+ * This method build the headers to make a request to the api
+ * @param token The auth token of the user
+ */
 function getHeaders(token?: string) {
 	let headers = new HttpHeaders({
 		'Content-Type': 'application/json'
