c34a63f28f08a55e2b78ae119c349c8b42919ec6
Daniel <danielrs9705@gmail.com>
2020-01-22 11:32:34 +0100

    Views of login and signup ready

--- src/app/sections/sign-up/sign-up.component.ts
+++ src/app/sections/sign-up/sign-up.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
-  selector: 'app-sign-up',
-  templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+	selector: 'app-sign-up',
+	templateUrl: './sign-up.component.html',
+	styleUrls: [ './sign-up.component.scss' ]
 })
 export class SignUpComponent implements OnInit {
+	signupForm: FormGroup;
+	constructor(private fb: FormBuilder, private usersService: UsersService, private router: Router) {}
 
-  constructor() { }
+	ngOnInit() {
+		this.signupForm = this.fb.group({
+			name: [ '' ],
+			email: [ '' ],
+			password: [ '' ]
+		});
+	}
 
-  ngOnInit() {
-  }
-
+	onSubmit(values) {
+		this.usersService.signup(values).subscribe(
+			() => {
+				this.router.navigate([ 'dashboard' ]);
+			},
+			(e) => console.log('An error ocurred!', e)
+		);
+	}
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { getHeaders } from '../utils/header';
+import { tap } from 'rxjs/operators';
+import { AuthService } from './auth.service';
 
 @Injectable({
 	providedIn: 'root'
@@ -11,12 +13,15 @@ export class UsersService {
 		signup: `${environment.apiServer}users/signup`
 	};
 
-	constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient, private auth: AuthService) {}
 
 	signup(form) {
 		const headers = getHeaders();
 		const body = form;
-		console.log(form);
-		// return this.http.post(this.apiUrls.signup, body, { headers });
+		return this.http.post(this.apiUrls.signup, body, { headers }).pipe(
+			tap((response) => {
+				this.auth.saveToken(response['token']);
+			})
+		);
 	}
 }
