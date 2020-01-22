374fb5d1804953264088af16efc6d5fba3d96dbf
Daniel <danielrs9705@gmail.com>
2020-01-22 11:37:52 +0100

    Create the auth guard for views that need it

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { CodeDocumentComponent } from '../sections/code-area/code-document/code-document.component';
 import { LoginComponent } from '../sections/login/login.component';
 import { SignUpComponent } from '../sections/sign-up/sign-up.component';
+import { AuthGuard } from '../_guards/auth.guard';
 
 const routes: Routes = [
-	{ path: 'document/:id', component: CodeDocumentComponent },
+	{ path: 'document/:id', canActivate: [ AuthGuard ], component: CodeDocumentComponent },
 	{ path: 'login', component: LoginComponent },
 	{ path: 'signup', component: SignUpComponent },
 	{ path: '**', redirectTo: 'document/123' }
