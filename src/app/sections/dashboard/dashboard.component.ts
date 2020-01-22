a5f53b17b8e9f365a9cfdecfda010256e4957bbe
angelicacosta <14-10005@usb.ve>
2020-01-22 15:00:39 +0100

    Adding buttons and pop ups

--- src/app/sections/dashboard/dashboard.component.ts
+++ src/app/sections/dashboard/dashboard.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
+
 export class DashboardComponent implements OnInit {
+  docs=[ 
+    {id:"jaajsaksd", nombre:"qlq", updateAt:new Date(),}
+  ]
+  user={ user_name:"Angelica", id:1 }/*
+  constructor( private router: Router, private api:ApiService) { }*/
 
-  constructor() { }
+  ngOnInit() { /*
+    this.api.getDocuments().subscribe((docs)=>{
+      console.log(docs);
+    });*/
+  }/*
 
-  ngOnInit() {
-  }
+  open_doc(id:string){
+    console.log(id);
+    this.router.navigate(["document",id]);
+  }*/
 
 }
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor( private http:HttpClient ) {  }
+
+  getDocuments() {
+    return this.http.get(environment.apiServer);
+  }
+}
